4bd21297e56bebe0450aa43c81c133b59fce4a27
divcamillediv <camille.divisia@hotmail.com>
2025-04-07 22:18:29 +0800

    in case it goes into production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
 import { app } from "./app.ts";
+import path from "path";
+import express, { Request, Response } from "express";
 
 export const startServer = ():void=>{
   dotenv.config();
   const PORT = process.env.PORT || 3000;
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req: Request, res: Response) => {
+      res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+  }
 
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
 }
